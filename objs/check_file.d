4794154b776fd1678a5b33d0eee41ec2ec088388
GaelGosse <gael.gosse@yahoo.fr>
2023-05-30 22:47:36 +0200

    week_en_27_05

--- objs/check_file.d
+++
@@ -1,5 +0,0 @@
-objs/check_file.o: srcs/check_file.c incs/cub3D.h incs/../libft/libft.h \
- incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/color.h:
--- objs/texture_part_utils.d
+++
@@ -1,5 +0,0 @@
-objs/texture_part_utils.o: srcs/texture_part_utils.c incs/cub3D.h \
- incs/../libft/libft.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/color.h:
