0faf61a2e2b1e6573f6502f500cbabb9c6241e88
GaelGosse <gael.gosse@yahoo.fr>
2023-06-09 16:20:35 +0200

    first_version_build_map

--- objs/free_parsing.d
+++
@@ -1,6 +0,0 @@
-objs/free_parsing.o: srcs/free_parsing.c incs/cub3D.h \
- incs/../libft/libft.h incs/../libft/../incs/color.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/../libft/../incs/color.h:
-incs/color.h:
