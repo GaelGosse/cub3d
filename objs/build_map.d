92f8fc76930aab5407860c11938c0df625d88b8a
GaelGosse <gael.gosse@yahoo.fr>
2023-06-13 08:56:36 +0200

    build_map_done

--- objs/build_map.d
+++
@@ -1,6 +0,0 @@
-objs/build_map.o: srcs/build_map.c incs/cub3D.h incs/../libft/libft.h \
- incs/../libft/../incs/color.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/../libft/../incs/color.h:
-incs/color.h:
--- objs/check_file.d
+++
@@ -1,6 +0,0 @@
-objs/check_file.o: srcs/check_file.c incs/cub3D.h incs/../libft/libft.h \
- incs/../libft/../incs/color.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/../libft/../incs/color.h:
-incs/color.h:
