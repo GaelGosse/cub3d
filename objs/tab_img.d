c883ef47f49c3388d52afe78c6c19d4c0d0bf0ba
Mlamarcq <mael.lamarcq0302@orange.fr>
2023-06-13 17:52:19 +0200

    push_for_make_mlx

--- objs/tab_img.d
+++
@@ -1,6 +0,0 @@
-objs/tab_img.o: srcs/tab_img.c incs/cub3D.h incs/../libft/libft.h \
- incs/../libft/../incs/color.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/../libft/../incs/color.h:
-incs/color.h:
--- objs/valid_wall.d
+++
@@ -1,6 +0,0 @@
-objs/valid_wall.o: srcs/valid_wall.c incs/cub3D.h incs/../libft/libft.h \
- incs/../libft/../incs/color.h incs/color.h
-incs/cub3D.h:
-incs/../libft/libft.h:
-incs/../libft/../incs/color.h:
-incs/color.h:
